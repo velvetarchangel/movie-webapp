fc2a8fea82e51859d332f840f6ff10b7944dea38
velvetarchange <h.dastidar@gmail.com>
2020-08-27 12:51:25 -0600

    Created a movie search feature

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 > The Shoppies </h1>
+      <div id="listSearchOuter">
+      <h2> Movie Titles</h2>
+      <input class="form-control" id="listSearchInner" type="text" placeholder="Search.." @input= "isTyping = true" v-model= "searchQuery">
+      <br>
+      </div>
+    <table id="resultTables">
+      <th>Results for "{{searchQuery}}"</th>
+      <th></th>
+      <th></th>
+      <th>Nomination list</th>
+        <tr>
+          <td id="searchResults">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="item in searchResult" :key="item">{{ item.Title }} ({{item.Year}})</li>
+            </ul>
+          </td>
+          <td id="nominateButton">
+            <ul>
+              <li class="list-group-item" v-for="item in searchResult" :key="item">
+                <button id="nomButton" @click="addNomination(item)"
+                  :disabled="nominated.includes(item)"
+                  >Nominate</button>
+              </li>
+            </ul>
+          </td>
+          <td id="spacer" style="width:5%; background-color: lightgrey"></td>
+          <td id="nominations">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="item in nominated" :key="item">{{ item.Title }} ({{item.Year}})</li>
+            </ul>
+          </td>
+          <td id="removeButton">
+            <ul>
+              <li class="list-group-item" v-for="item in nominated" :key="item">
+                <button id="remButton" @click="removeNomination(item, nominated.indexOf(item))">Remove</button>
+              </li>
+            </ul>
+          </td>
+        </tr>
+    </table>
+    <div id = "banner" v-if="maxNomination()">
+      Maximum number of nominations reached
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import _ from 'lodash'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      searchQuery: "",
+      isTyping: false,
+      isLoading: false,
+      searchResult: [],
+      nominated: [],
+    }
+  },
+  watch: {
+    searchQuery: _.debounce( function() {
+      this.isTyping = false;
+    }, 1000),
+    isTyping: function(value) {
+      if (!value) {
+        this.search(this.searchQuery);
+      }
+    }
+  },
+  methods: {
+    search: function(searchQuery) {
+      this.isLoading = true;
+      axios
+      .get('http://www.omdbapi.com/?apikey=324544c2&s=' + searchQuery)
+      .then(response => {
+        this.isLoading = false;
+        console.log(response);
+        this.searchResult = response.data.Search;
+        this.nominatedDisabled = true;
+      })
+    },
+
+    addNomination: function (item) {
+      if (!this.nominated.includes(item)){
+        this.nominated.push(item);
+      }
+    },
+
+    removeNomination: function(item, index) {
+      this.nominated.splice(index, 1);
+    },
+    
+    maxNomination: function () {
+      if (this.nominated.length === 5) {
+        this.searchQuery = "";
+        this.isTyping = false;
+        this.isLoading = false;
+        return true;
+      } else if (this.nominated.length < 5) {
+        return false;
+      } else {
+        this.nominated = [];
+        this.searchQuery = "";
+        this.searchResult = [];
+        this.isTyping = false;
+        this.isLoading = false;
+        return false;
+      }
+    }
   }
 }
 </script>
@@ -24,5 +126,117 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #cccccc;
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+
+#listSearchOuter {
+  border-radius: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 20px;
+  padding-bottom: 30px;
+  background-color: white;
+  width: 90%;
+  text-align:left;
+}
+
+h2 {
+  padding-top: 10px;
+}
+
+#listSearchInner {
+  background-color: white;
+  width: 90%;
+  height: 30px;
+}
+
+.list-group-item {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+table{
+
+  width: 92%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top:30px;
+  border-spacing: 0px;
+}
+
+.resultsTable td {
+  padding: 0 10px 0 0;
+}
+
+#listGroup {
+  background-color: white;
+}
+
+#searchResults {
+  background-color: white;
+  text-align: left;
+  width: 45%;
+}
+
+#nominations {
+  background-color: white;
+  text-align: left;
+  width: 45%;
+}
+
+#spacer {
+  width: 5%;
+}
+
+tr {
+  background-color: white;
+}
+
+ul {
+  list-style-type: none;
+}
+
+th {
+  text-align: center;
+}
+
+#nomButton {
+  border-radius: 5px;
+  background-color:green;
+  color:white;
+  font-weight: bold;
+}
+
+#remButton {
+  border-radius: 5px;
+  background-color:lightcoral;
+  color:white;
+  font-weight: bold;
+}
+
+button:disabled {
+  align-self: left;
+  border-radius: 5px;
+  background-color:green;
+  color:white;
+  opacity:30%;
+  font-weight: bold;
+}
+
+#banner {
+  padding-top: 5px;
+  width: 92%;
+  text-align: center;
+  color: black;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  opacity: 60%;
+  background-color: #66ffc2;
+  border: 1px solid #003300;
+  border-radius: 25px;
 }
 </style>
